8fce4ce350da31cdf2dc46682b7f68f53cef9e80
RAKHYUN <kyle2598@gmail.com>
2023-11-07 14:56:29 -0600

    Initial Commit

---
+++ Fetch project/Fetch_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_projectApp.swift
+//  Fetch project
+//
+//  Created by Rakhyun Kim on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
